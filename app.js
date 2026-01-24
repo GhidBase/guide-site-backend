776a47347bcb7d191e3abdc3c6429fe0b3b7a818
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-24 22:02:40 +0400

    feat: allow credentials to be passed in with CORS

--- app.js
+++ app.js
@@ -39,7 +39,12 @@ app.use(
 app.use(passport.session());
 
 import cors from "cors";
-app.use(cors());
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+        credentials: true,
+    }),
+);
 
 // To receive JSON
 app.use(express.json());
