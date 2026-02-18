d7055171b5029a7407561556243de528bbf50265
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-02-19 01:05:13 +0400

    feat: get, accept, and refuse pending reviews

---
+++ routes/reviewRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import reviewController from "../controllers/reviewController.js";
+import { requireEditor } from "../config/auth.js";
+
+const router = Router();
+router.get("/", requireEditor, reviewController.getAllPendingReviews);
+router.post("/:id/accept", requireEditor, reviewController.acceptReview);
+router.post("/:id/refuse", requireEditor, reviewController.refuseReview);
+
+export default router;
